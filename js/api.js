2f86d98d84a5d07546335a7dea735e90980bfe7a
Ahmad-Said-2350 <mail.saydorrahman@gmail.com>
2026-03-07 02:06:54 -0800

    All issues UI , appended complet

--- js/api.js
+++ js/api.js
@@ -12,14 +12,36 @@
     issues.forEach(issues => {
 
         const card = document.createElement("div");
+        
+        card.classList.add("card", "bg-base-100" ,"w-full", "shadow-sm", 'min-h-[280px]')
         card.innerHTML = `
                 
-        <p>${issues.priority}</p>
-        <h2>${issues.title}</h2>
-        <p>${issues.description}</p>
-        <p>${issues.labels}</p>
-        <p>${issues.author}</p>
-        <p>${issues.updatedAt}</p>
+        <div class="">
+  
+<div class="card-body">
+<div class="show flex justify-between items-center ">
+
+    <img src="./assets/Open-Status.png" alt="">
+    <div class= " badge badge-outline badge-warning">${issues.priority}</div>
+
+    
+</div>
+
+<h2 class="text-2xl font-bold text-[#1F2937]">${issues.title}</h2>
+<p class=" text-base text-[#64748B] line-clamp-2">${issues.description}</p>
+<p class="badge badge-outline badge-error">${issues.labels}</p>
+
+  <div class="divider my-2"></div>
+
+<p class="text-base text-[#64748B] ">${issues.author}</p>
+<p class="text-base text-[#64748B] ">${issues.createdAt}</p>
+    
+  </div>
+
+</div>
+
+
+
         `
 
 
